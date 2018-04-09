a22b100baf661bbf39aea996e6fc524cd2a08d16
bkdpchaturvedi <37078558+bkdpchaturvedi@users.noreply.github.com>
2018-04-09 19:50:49 +0800

    inital commit

---
+++ NUS.TheAmagingRace.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NUS.TheAmagingRace.MVC.MvcApplication" Language="C#" %>
---
+++ NUS.TheAmagingRace.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NUS.TheAmagingRace.MVC.Startup))]
+namespace NUS.TheAmagingRace.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
