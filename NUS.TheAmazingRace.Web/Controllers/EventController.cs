7358a5bb76feb81c94b9acd6c9d9263bdb895c09
unknown <Gowtham@DESKTOP-IBBO55K.(none)>
2018-04-10 17:26:43 +0800

    Added Event Controller

---
+++ NUS.TheAmazingRace.Web/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NUS.TheAmazingRace.Web.Controllers
+{
+    [Authorize]
+    [ValidateAntiForgeryToken]
+    public class EventController : Controller
+    {
+        // GET: Event
+        [ActionName("EventLists")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
