d5d7c5181122060a222031e959ac1c74ba2b176a
bkdpchaturvedi <37078558+bkdpchaturvedi@users.noreply.github.com>
2018-04-13 03:42:35 +0800

    commiting master layouts

--- NUS.TheAmazingRace.Web/App_Start/BundleConfig.cs
+++ NUS.TheAmazingRace.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace NUS.TheAmazingRace.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+           
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -24,8 +25,11 @@ namespace NUS.TheAmazingRace.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/SideBar.css"
+                      ));
+
+          
         }
     }
 }
