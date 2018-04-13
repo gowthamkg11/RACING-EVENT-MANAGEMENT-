30afc2fc827c44a16eb4f39d4738b23b7d05c7a4
unknown <Gowtham@DESKTOP-IBBO55K.(none)>
2018-04-13 19:39:09 +0800

    Used BAL layer for Events

---
+++ NUS.TheAmagingRace.BAL/EventBAL.cs
@@ -0,0 +1,70 @@
+﻿using NUS.TheAmagingRace.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUS.TheAmagingRace.BAL
+{
+    public class EventBAL
+    {
+        private TARDBContext db = new TARDBContext();
+        private Event events = new Event();
+
+        public List<Event> GetEventList()
+        {
+            return db.Events.ToList(); ;
+        }
+
+        public List<Event> EditEventList(Event eventModel)
+        {
+            if (eventModel.EventID > 0)
+            {
+                Event editEvents = db.Events.SingleOrDefault(x => x.EventID == eventModel.EventID);
+                editEvents.EventName = eventModel.EventName;
+                editEvents.EventDescription = eventModel.EventDescription;
+                editEvents.EventCountry = eventModel.EventCountry;
+                editEvents.EventCity = eventModel.EventCity;
+                editEvents.StartDate = eventModel.StartDate;
+                editEvents.EndDate = eventModel.EndDate;
+                editEvents.TotalPitStops = eventModel.TotalPitStops;
+                editEvents.TotalTeams = eventModel.TotalTeams;
+                db.SaveChanges();
+            }
+            else
+            {
+                db.Events.Add(eventModel);
+                db.SaveChanges();
+            }
+
+            return db.Events.ToList(); ;
+        }
+
+        public Event GetEditingValues(int eventId)
+        {
+            Event editEvents = db.Events.SingleOrDefault(x => x.EventID == eventId);
+            events.EventID = editEvents.EventID;
+            events.EventName = editEvents.EventName;
+            events.EventDescription = editEvents.EventDescription;
+            events.EventCountry = editEvents.EventCountry;
+            events.EventCity = editEvents.EventCity;
+            events.StartDate = editEvents.StartDate;
+            events.EndDate = editEvents.EndDate;
+            events.TotalPitStops = editEvents.TotalPitStops;
+            events.TotalTeams = editEvents.TotalTeams;
+            return events;
+
+        }
+
+        public bool DeleteEventfromList(int eventId)
+        {
+            bool result = false;
+            var events = db.Events.Find(eventId);
+            db.Events.Remove(events);
+            db.SaveChanges();
+            result = true;
+            return result;
+        }
+    }
+}
--- NUS.TheAmazingRace.Web/Controllers/EventController.cs
+++ NUS.TheAmazingRace.Web/Controllers/EventController.cs
@@ -1,4 +1,5 @@
-﻿using NUS.TheAmagingRace.DAL;
+﻿using NUS.TheAmagingRace.BAL;
+using NUS.TheAmagingRace.DAL;
 using NUS.TheAmazingRace.Web.Models;
 using System;
 using System.Collections.Generic;
@@ -6,45 +7,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace NUS.TheAmazingRace.Web.Controllers
 {
     [Authorize]
 
     public class EventController : Controller
     {
-        // GET: Event
-        private TARDBContext db = new TARDBContext();
-        private Event events = new Event();
+      
+       
+        private EventBAL eventBAL = new EventBAL();
+
 
 
         [HttpGet]
         public ActionResult Index()
         {
-
-            return View(db.Events.ToList());
+            return View(eventBAL.GetEventList());
         }
+
+
         [HttpPost]
         public ActionResult Index(Event eventModel)
         {
-            if (eventModel.EventID > 0)
-            {
-                Event editEvents = db.Events.SingleOrDefault(x => x.EventID == eventModel.EventID);
-                editEvents.EventName = eventModel.EventName;
-                editEvents.EventDescription = eventModel.EventDescription;
-                editEvents.EventCountry = eventModel.EventCountry;
-                editEvents.EventCity = eventModel.EventCity;
-                editEvents.StartDate = eventModel.StartDate;
-                editEvents.EndDate = eventModel.EndDate;
-                editEvents.TotalPitStops = eventModel.TotalPitStops;
-                editEvents.TotalTeams = eventModel.TotalTeams;
-                db.SaveChanges();
-            }
-            else
-            {
-                db.Events.Add(eventModel);
-                db.SaveChanges();
-            }
-           return View(db.Events.ToList());
+           return View(eventBAL.EditEventList(eventModel));
         }
 
 
@@ -56,29 +42,14 @@ namespace NUS.TheAmazingRace.Web.Controllers
 
         public JsonResult DeleteEvent(int eventId)
         {
-            bool result = false;
-            var events = db.Events.Find(eventId);
-            db.Events.Remove(events);
-            db.SaveChanges();
-            result = true;
-
+            bool result = eventBAL.DeleteEventfromList(eventId);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult EditEvent(int eventId)
         {
-
-            Event editEvents = db.Events.SingleOrDefault(x => x.EventID == eventId);
-            events.EventID = editEvents.EventID;
-            events.EventName = editEvents.EventName;
-            events.EventDescription = editEvents.EventDescription;
-            events.EventCountry = editEvents.EventCountry;
-            events.EventCity = editEvents.EventCity;
-            events.StartDate = editEvents.StartDate;
-            events.EndDate = editEvents.EndDate;
-            events.TotalPitStops = editEvents.TotalPitStops;
-            events.TotalTeams = editEvents.TotalTeams;
-            return PartialView("_EditEvent", events);
+                        
+            return PartialView("_EditEvent", eventBAL.GetEditingValues(eventId));
 
         }
 
